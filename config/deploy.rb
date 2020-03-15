01446cf004ac02c069a17c1a7fca77ea8c2e6bfb
toannt181 <toannt181@gmail.com>
2020-03-15 20:38:21 +0700

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,86 +1,25 @@
-# Change these
-server '159.65.15.210', port: 22, roles: [:web, :app, :db], primary: true
+# config valid only for current version of Capistrano
+lock "3.5.0"
 
-set :repo_url,        'git@github.com:toannt181/railway.git'
-# Example: git@example.com:username/appname.git
+set :application, "railway"
+set :repo_url,  'git@github.com:toannt181/railway.git'
+set :pty, true
+set :linked_files, %w(config/database.yml config/application.yml)
+set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
+set :keep_releases, 5
+set :rvm_type, :user
 
-set :application,     'railway'
-set :user,            'toan'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
-
-# Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
+set :puma_rackup, -> {File.join(current_path, "config.ru")}
+set :puma_state, -> {"#{shared_path}/tmp/pids/puma.state"}
+set :puma_pid, -> {"#{shared_path}/tmp/pids/puma.pid"}
+set :puma_bind, -> {"unix://#{shared_path}/tmp/sockets/puma.sock"}
+set :puma_conf, -> {"#{shared_path}/puma.rb"}
+set :puma_access_log, -> {"#{shared_path}/log/puma_access.log"}
+set :puma_error_log, -> {"#{shared_path}/log/puma_error.log"}
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, "staging"))
+set :puma_threads, [0, 8]
+set :puma_workers, 0
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
-
-## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml config/master.key}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
-end
-
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
+set :puma_init_active_record, true
+set :puma_preload_app, false
